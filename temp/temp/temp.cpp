0a9373af7b6b955d5c3fff4beb925db094c7ac05
Hui Jiang <814775538@qq.com>
2019-12-19 13:52:31 +0800

    exam2

---
+++ temp/temp/temp.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <ctime>
+#include <cstdlib>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <algorithm>
+#include <windows.h>
+#include <iterator>//这是output.cpp里引用的，可以做输入输出的流迭代器
+#include <limits>//这是variable.cpp里引用的，获取一些变量类型的属性
+#include <locale.h>
+
+
+# define max(a, b) (a) > (b) ? (a) : (b)
+# define min(a, b) (a) < (b) ? (a) : (b)
+
+using namespace std;
+
+
+int main() {
+
+	return 0;
+}
